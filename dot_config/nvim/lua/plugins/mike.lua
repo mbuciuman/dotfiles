0e8c598c15c5a1e02b6fa34a7bfff703e018cbcb
Michael Buciuman <mbuciuma@gmail.com>
2025-09-27 15:05:48 +0800

    Removed unused plugin files

--- dot_config/nvim/lua/plugins/mike.lua
+++
@@ -1,8 +0,0 @@
-return {
-    { 'nvim-lualine/lualine.nvim', enabled = true },
-    { 'tpope/vim-fugitive' },
-    { 'scrooloose/syntastic' },
-    { 'vim-airline/vim-airline' },
-    { 'vimwiki/vimwiki' },
-    { 'lukas-reineke/indent-blankline.nvim' },
-}
